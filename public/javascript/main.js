45fd15fd6c39d0b888ea9e8853337439dfa67999
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-11 21:47:26 +0000

    building a tree slowly

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -14,15 +14,16 @@ function loadFollowers(username){
   $.get('https://api.github.com/users/'+username+'/followers',function(followers){
     centerUserFollowers = followers;
 
+    //i have all objects ready so maybe it would be better to create a list of objects
+
     for(var i = 0;i < centerUserFollowers.length; i++){ 
       console.log(centerUserFollowers[i].login);
-      followers_list.push(centerUserFollowers[i].login);
+      followers_list.push(centerUserFollowers[i]);
     }
   });
 
 
   user = new User();
-  user.login = username;
   user.followers = followers_list;
 }
 
--- public/javascript/user.js
+++ public/javascript/user.js
@@ -1,5 +1,4 @@
 function User(){
-  this.login = '';
   this.followers = undefined;
   this.following = undefined;
 };
