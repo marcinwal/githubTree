4164dcd349c7c18c95682e9f197df35714719263
marcin walendzik <marcin.walendzik@gmail.com>
2015-02-16 07:47:37 +0000

    working without callback to do;delays

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,29 +1,61 @@
+// var GitHubApi = require("github");
 var centerUser;
 var centerUserFollowers;
 var user;
+var networkAllUsers= [];
 
+// github.authenticate({
+//     type: "oauth",
+//     key: "31b2fd9fbe37af7c3ae6",
+//     secret: "52b79f74b10c64a3476b6e4730e17d58e6c70110"
+// })
+
+function loadUser(username){
+  user = new Node();
+  $.get('https://api.github.com/users/'+ username,function(user_reply){
+      user.info = user_reply;
+      networkAllUsers.push(user.info.login); 
+  });
+  $('#formdepth').attr('class','formshow');
+}
 
 function loadFollowers(user,field){
   var followers_list=[]
-  $.get('https://api.github.com/users/'+user.info[field]+'/followers',function(followers){
+  path = 'https://api.github.com/users/'+user.info[field]+'/followers';
+  $.get(path,function(followers){
     for(var i = 0;i < followers.length; i++){ 
       user.addFollower(followers[i]);
     };
   });
 }
 
-function loadUser(username){
-  user = new Node();
-  $.get('https://api.github.com/users/'+ username,function(user_reply){
-      user.info = user_reply; 
-  });
-  $('#formdepth').attr('class','formshow');
+function loadNetwork(node,depth,field){
+  if (depth == 0)
+  {
+    return;
+  }else
+  {
+    loadFollowers(node,field);
+    depth -=1;
+    for(var i=0; i < node.followers.length; i++){
+      current = node.followers[i];
+      if (networkAllUsers.indexOf(current.info.field)===-1)
+      { //searchin one more if not searched previously
+        networkAllUsers.push(current.info[field]);
+        loadNetwork(current,depth,field);
+      }  
+    };
+    return;
+  }
 }
+
 
 $('#formdepth').on('submit',function(event){
   event.preventDefault();
   console.log(event)
-  loadFollowers(user,'login');
+  depth = $('#depth').val();
+  // loadFollowers(user,'login');
+  loadNetwork(user,depth,'login')
 });
 
 $(document).ready(function(){
