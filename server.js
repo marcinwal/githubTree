0c60e0d476ac66cb18dfff4d395c6fbbc7de3943
marcin walendzik <marcin.walendzik@gmail.com>
2015-05-15 18:43:34 +0100

    server start

--- server.js
+++ server.js
@@ -4,23 +4,25 @@ var http = require('http');
 
 var server = http.createServer(app);
 
-var port = 9999;
+var portLocal = 3000;
 
+app.set('port',process.env.PORT||portLocal);
 app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/',function(req,res){
-  res.send('Hello World!');
+  // res.render('index');
+  res.send('Hello');
 });
 
 app.get('/users/:user', function(request,response) {
   console.log(request.params.user);
   response.header('Access-Control-Allow-Origin','*');
   response.header('Content-Type', 'application/json; charset=utf-8');
-  // response.render(request.params.user);
+  response.render(request.params.user);
 });
 
 
-server.listen(port,function(){
-  console.log("running server on port "+port);
+server.listen(app.get('port'),function(){
+  console.log("running server on port:"+app.get('port'));
 });
--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,9 @@
 var chai = require('chai');               
 var expect = chai.expect;  
+var casper = require('casperjs');
 
 describe('basic test',function(){
-     it('should to simple',function(){
+     it('should do simple',function(){
           expect(1+1).to.equal(2);
      });
 });
